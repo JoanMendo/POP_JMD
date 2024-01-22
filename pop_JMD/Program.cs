c439656056b76c89a40858bcd05d51aeb8baf788
Mendo <joan.mendo.7e7@itb.cat>
2024-01-22 11:44:18 +0100

    Closes #1

--- pop_JMD/Program.cs
+++ pop_JMD/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography;
+namespace PoPRefactoring;
+
+public class PopProject
+{
+    public static void Main(string[] args)
+    {
+        int dia, mes, any;
+
+        bool validat;
+        Console.WriteLine("Introdueix el dia, mes i any");
+        dia=Convert.ToInt32(Console.ReadLine());
+        mes= Convert.ToInt32(Console.ReadLine());
+        any = Convert.ToInt32(Console.ReadLine());
+        validat = valida(dia, mes, any);
+        string message = (validat) ? "La data és correcta " : "El format no és correcte";
+        Console.WriteLine(message);
+        
+    }
+
+    public static bool valida(int day, int month, int year)
+    {
+
+        if (day < 1 || day > 31)
+
+            return false;
+
+        if (month < 1 || month > 12)
+
+            return false;
+
+        // determinem la quantitat de dies del mes:
+
+        int totalDaysMonth = 0;
+
+        switch (month)
+        {
+
+            case 1: totalDaysMonth = 31; break;
+
+            case 2: // verifica l'any de traspàs
+
+                if ((year % 400 == 0) ||
+
+                ((year % 4 == 0) && (year % 100 != 0)))
+                    totalDaysMonth = 29;
+
+                else totalDaysMonth = 28;
+
+                break;
+
+            case 3: totalDaysMonth = 31; break;
+
+            case 4: totalDaysMonth = 30; break;
+
+            case 5: totalDaysMonth = 31; break;
+
+            case 6: totalDaysMonth = 30; break;
+
+            case 7: totalDaysMonth = 31; break;
+
+            case 8: totalDaysMonth = 31; break;
+
+            case 9: totalDaysMonth = 30; break;
+
+            case 10: totalDaysMonth = 31; break;
+
+            case 11: totalDaysMonth = 30; break;
+
+
+            case 12: totalDaysMonth = 31; break;
+
+
+
+        }
+
+        if (day > totalDaysMonth)
+
+            return false;
+
+        else return true;
+
+    }
+}
